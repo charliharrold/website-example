46cedae640876091271ac1e6403f273e62a57dea
charliharrold <charliharrold@gmail.com>
2025-01-15 21:03:23 -0800

    Make on page and a navigation bar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const makePink = {
+    "background-color": "#fec5e5",
+}
+
+function Navigation() {
+  const [activeKey, setActiveKey] = useState('#home');
+
+  const handleBrandClick = () => {
+    setActiveKey('#home');
+  };
+
+  return (
+    <Navbar expand="lg" style={makePink}>
+      <Container>
+        <Navbar.Brand href="#home"  onClick={handleBrandClick}>Charli Harrold</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto" activeKey={activeKey} onSelect={(selectedKey) => setActiveKey(selectedKey)}>
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
